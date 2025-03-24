ec2fce5605e07f620f789f2d3c2df328b9871e00
Jyoti Sharma <sharmajyoti77594@gmail.com>
2025-03-25 00:43:33 +0530

    Navigation bar setup

--- frontend/src/components/NavbarCustom.jsx
+++ frontend/src/components/NavbarCustom.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from "react-router";
 
 function NavbarCustom () {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" bg="success" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <NavLink to='/' className={'navbar-brand fw-bold fst-italic'} 
+                        style={({isActive}) => ({ color : 'white'})}>GoFood</NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='about'>About</NavLink>
           </Nav>
         </Navbar.Collapse>
+
       </Container>
     </Navbar>
   );
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -5,8 +5,7 @@ import Footer from '../components/Footer'
 function Home() {
   return (
     <>
-      <Navbar/>
-      <Footer/>
+      Home
     </>
   )
 }
